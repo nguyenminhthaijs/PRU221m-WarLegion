736d5a539cd21e5a99290d42f15072354dfe955e
Tuan Thanh <tuanthanhnghean123@gmail.com>
2023-02-24 22:02:01 +0700

    Coding the basic health bar without assign game characters

---
+++ Assets/Scripts/Gameplay/HealthBar.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField]
+    Slider slider;
+    public Gradient gradient;
+    public Image fill;
+    public void SetMaxHealth(int health)
+    {
+        slider.value = health;
+        slider.maxValue = health;
+        fill.color = gradient.Evaluate(1f);
+    }
+    public void SetHealth(int health)
+    {
+        slider.value = health;
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+    }
+}
