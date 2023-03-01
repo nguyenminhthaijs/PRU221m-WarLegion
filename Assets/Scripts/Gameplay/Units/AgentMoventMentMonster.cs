718d4e0b1e8d2609b2e63153b0c35a21de385368
Tuan Thanh <tuanthanhnghean123@gmail.com>
2023-03-02 01:36:01 +0700

    Attacker move to tower and attack defender to death

--- Assets/Scripts/Gameplay/Units/AgentMoventMentMonster.cs
+++ Assets/Scripts/Gameplay/Units/AgentMoventMentMonster.cs
@@ -11,9 +11,8 @@ namespace Assets.Scripts.Gameplay.Units
     public class AgentMoventMentMonster : MonoBehaviour
     {
         Vector3 target;
-        public bool isMoving = false;
 
-        public bool isAccessMovingTower = true; 
+        public bool isAccessMovingTower; 
         // Chi co 1 tru thi khai bao day luon 
         public GameObject tower; 
 
@@ -24,31 +23,27 @@ namespace Assets.Scripts.Gameplay.Units
             agent = GetComponent<NavMeshAgent>();
             agent.updateRotation = false;
             agent.updateUpAxis = false;
+            // init target to tower
             agent.SetDestination(tower.transform.position);
-            isAccessMovingTower = true;
+            isAccessMovingTower = false;
         }
 
         // Update is called once per frame
         void Update()
         {
-            // Cu den muc tieu thi set target den tower
+            // Chua den tru thi cu set target va co duoc Access Moving den Tower hay khong ( dang target defender thi k di chuyen den tower )
             // Truong hop den roi thi khong phai set nua
             if (Vector2.Distance(gameObject.transform.position, tower.transform.position) > 0.1f && isAccessMovingTower == true)
             {
-                Debug.Log(Vector2.Distance(gameObject.transform.position, tower.transform.position));
                 agent.SetDestination(tower.transform.position);
                 Debug.Log("Target tower");
-            }
-            else
-            {
-                isMoving = false;
+                isAccessMovingTower = false;
             }
         }
         public void SetTargetPosition(GameObject t)
         {
             Transform transform = t.transform; 
             agent.SetDestination(transform.position);
-            isMoving = true;
         }
         public void StopMoving()
         {
@@ -57,18 +52,6 @@ namespace Assets.Scripts.Gameplay.Units
         public void ContinueMoving()
         {
             agent.isStopped = false;
-        }
-        private void OnTriggerEnter2D(Collider2D collision)
-        {
-            //Debug.Log("Find enemies");
-        }
-        public bool CheckIsMoving()
-        {
-            return isMoving;
-        }
-        void SetAgentPosition()
-        {
-
         }
     }
 }
